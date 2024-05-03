3112e0af1d3b2d4bca8703e8e7f928b086c88814
root <root@DESKTOP-40K4BTE.localdomain>
2024-05-03 12:18:19 +0300

    fixing file 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # installi flask
 package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+  ensure               => '2.1.0',
+  provider             => 'pip3',
   reinstall_on_refresh => true,
 }
