058736f9b66b05fdc3a14406cdf4d03344f8e98f
root <root@DESKTOP-40K4BTE.localdomain>
2024-05-05 06:33:59 +0300

    adding file 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,5 +2,5 @@
 file { '/root/.ssh/config':
   ensure  => 'present',
   mode    => '0600',
-  content => 'Host \n\t IdentityFile ~/.ssh/school\n\t PasswordAuthentication no',
+  content => 'Host \n\t FileName ~/.ssh/school\n\t PasswordAuthentication no',
   }
