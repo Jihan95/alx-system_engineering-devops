42828272c55e48977339f093ab9c4ea2fd54163a
root <root@DESKTOP-40K4BTE.localdomain>
2024-05-05 06:32:05 +0300

    adding file 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# Client Configuration file
+file { '/root/.ssh/config':
+  ensure => 'present',
+  mode => '0600',
+  content => 'Host \n\t IdentityFile ~/.ssh/school\n\t PasswordAuthentication no',
+  }
