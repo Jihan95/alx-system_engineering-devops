c5cb7875e58debedd134417a1dabde9c53ec3a0e
root <root@DESKTOP-40K4BTE.localdomain>
2024-05-05 22:21:49 +0300

    update 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,16 @@
 # Client Configuration file
-File_line { 'Turn off passwd auth':
-  path    => '/root/.ssh/config',
-  line    => 'PasswordAuthentication no',
-  match   => '^PasswordAuthentication',
+file_line { 'Turn off passwd auth':
+  path  => '/root/.ssh/config',
+  line  => 'PasswordAuthentication no',
+  match => '^PasswordAuthentication',
 }
 
-File_line { 'Declare identity file':
-  path    => '/root/.ssh/config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^IdentityFile',
+file_line { 'Declare identity file':
+  path  => '/root/.ssh/config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^IdentityFile',
 }
+
+
 
 
