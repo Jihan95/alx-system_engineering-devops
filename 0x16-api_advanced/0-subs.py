a9eb2eaec3f28f805fcea35fd05b80a77cf9808b
Jihan <jihanahmed41@gmail.com>
2024-06-19 22:33:25 -0700

    initial commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,13 +5,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ function that returns numbers of subs """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = f"https://oauth.reddit.com/r/{subreddit}/about.json"
     headers = {
         'User-Agent': 'MyRedditApp/1.0'
     }
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        print(response.status_code)
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
