4dd61fccba366c518e5bb40dcb7489226923c92e
Jihan <jihanahmed41@gmail.com>
2024-06-19 22:30:54 -0700

    initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" How many subs? """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ function that returns numbers of subs """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        'User-Agent': 'MyRedditApp/1.0'
+    }
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        print(response.status_code)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
